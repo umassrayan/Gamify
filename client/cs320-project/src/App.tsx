fc7a711770f5fd94cbdcc8a0b7cd10b48baac500
Heather Newandee <hnewandee@umass.edu>
2025-04-22 13:30:35 -0400

    Created the Welcome Page, Sign in, and Create Account components

--- client/cs320-project/src/App.tsx
+++ client/cs320-project/src/App.tsx
@@ -1,10 +1,21 @@
-import Calendar from "./components/Calendar";
+// import Calendar from "./components/Calendar";
+import WelcomePage from "./components/WelcomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div>
-      <Calendar></Calendar>
-    </div>
+    // <div>
+    //   <Calendar></Calendar>
+    // </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
   );
 }
 
