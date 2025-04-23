390e72d31b5621ac06ed0ced870ed375c9974071
Iris Gao <gaomuqing0407@gmail.com>
2025-04-23 11:45:50 -0400

    sidebar

--- client/cs320-project/src/App.tsx
+++ client/cs320-project/src/App.tsx
@@ -1,4 +1,3 @@
-// import Calendar from "./components/Calendar";
 import WelcomePage from "./components/WelcomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
@@ -6,30 +5,32 @@ import SignUp from "./components/SignUp";
 import React from "react";
 import AssignmentBoard from "./components/Assignment";
 import Calendar from "./components/Calendar";
+import Sidebar from "./components/Sidebar";
 
 const App: React.FC = () => {
   return (
-    // <div>
-    //   <Calendar></Calendar>
-    // </div>
     <Router>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div
+          style={{
+            flex: 1,
+            fontFamily: "sans-serif",
+            backgroundColor: "#ece8e4",
+            minHeight: "100vh",
+            padding: "40px",
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<WelcomePage />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/assignments" element={<AssignmentBoard />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
-    <div
-      style={{
-        fontFamily: "sans-serif",
-        backgroundColor: "#ece8e4",
-        minHeight: "100vh",
-        padding: "40px",
-      }}
-    >
-      <Calendar></Calendar>
-      <AssignmentBoard />
-    </div>
   );
 };
 
