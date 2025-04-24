00ade5e43b53fc877f93759fa825a4b31eded498
Heather Newandee <hnewandee@umass.edu>
2025-04-24 15:15:40 -0400

    fixed merge conflict

--- client/cs320-project/src/App.tsx
+++ client/cs320-project/src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import SignIn from "./components/SignIn";
 // import SignUp from "./components/SignUp";
 
+// import Sidebar from "./components/Sidebar";
+
 import Calendar from "./components/Calendar";
 import AssignmentBoard from "./components/Assignment";
 
@@ -55,34 +57,35 @@ const App: React.FC = () => {
         />
         <Route path="/AccountSettings" element={<AccountSettings />} />
       </Routes>
-
-import Sidebar from "./components/Sidebar";
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <div style={{ display: "flex" }}>
-        <Sidebar />
-        <div
-          style={{
-            flex: 1,
-            fontFamily: "sans-serif",
-            backgroundColor: "#ece8e4",
-            minHeight: "100vh",
-            padding: "40px",
-          }}
-        >
-          <Routes>
-            <Route path="/" element={<WelcomePage />} />
-            <Route path="/signin" element={<SignIn />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/calendar" element={<Calendar />} />
-            <Route path="/assignments" element={<AssignmentBoard />} />
-          </Routes>
-        </div>
-      </div>
     </Router>
   );
 };
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <div style={{ display: "flex" }}>
+//         <Sidebar />
+//         <div
+//           style={{
+//             flex: 1,
+//             fontFamily: "sans-serif",
+//             backgroundColor: "#ece8e4",
+//             minHeight: "100vh",
+//             padding: "40px",
+//           }}
+//         >
+//           <Routes>
+//             <Route path="/" element={<WelcomePage />} />
+//             <Route path="/signin" element={<SignIn />} />
+//             <Route path="/signup" element={<SignUp />} />
+//             <Route path="/calendar" element={<Calendar />} />
+//             <Route path="/assignments" element={<AssignmentBoard />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// };
 
 export default App;
